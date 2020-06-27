aac5894eb3c209fe5af1190e49a7b10ef3588dbd
farahalshukry-485 <farahshukry@gmail.com>
2020-06-27 20:53:19 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,27 +1,31 @@
 // السؤال الأول
 // 1
-
+const name="farah";
 // 2
-
+let age=17;
 // 3 and 4
-
+console.log(  name+ " "+ age);
 // 5 and 6
-
+console.log( age*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies =["sfx makeup",] ;
 // 2
-
+hobbies.push(age);
+console.log ( hobbies)
 // 3
-
+hobbies.pop(age);
+console.log( hobbies)
 // 4
-
+ let student = {track : " web", favourite : " JAVASCRIPT "} 
+ console.log( student)
 // 5
-
-// 6
-
+student.TA = "Rahaf" ;
+console.log( student)
+// // 6
+console.log(student.track)
 // 7
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
